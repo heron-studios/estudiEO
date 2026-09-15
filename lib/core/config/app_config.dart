ffc5bac7d28357a15d3c0504a033a515d221ac02
ELRUÑO <brizq02@gmail.com>
2026-09-14 20:31:32 -0500

    fix: update Android APK download link to edupol-release-36

--- lib/core/config/app_config.dart
+++ lib/core/config/app_config.dart
@@ -18,7 +18,7 @@ class AppConfig {
 
   /// URL oficial de descarga directa para la última versión del APK de Android.
   static const String androidApkDownloadUrl =
-      'https://download1475.mediafire.com/h4gzkl223wxgZPUmgBnHQAT7rn3o5T8nyjg_NILtOEWpl43suUPD8fKOxadNnW1_5j5Oa1QPnSiUlfbJHBmqoPyj0KIUz7HIEmgkr4Pqo3cf0k8pgiK3lN30UNzhFgdYsjfgAWmh5MjCJWGJ3ZO7EIqOVUNgtwQxPtDNU9aBgiEgiA/rpen8pn1vkkm0kh/edupol-release.apk';
+      'https://www.mediafire.com/file/llp1wxsqrs6pl9y/edupol-release-36.apk/file';
 
   /// URL oficial de descarga e instalación desde Google Play Store.
   static const String playStoreUrl =
